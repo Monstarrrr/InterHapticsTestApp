9871aafb4751893a4f02098a2cd912bf4062b9e9
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2024-10-01 18:04:54 +0200

    Init react

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import React from "react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <h1>Hi !!!</h1>
+  </React.StrictMode>
+);
